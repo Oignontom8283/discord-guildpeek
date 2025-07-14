726dc1f86fe0f19c15deac2462e564ae931d5f61
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-07-15 00:58:43 +0200

    Add functions for extracting Discord invite IDs and retrieving invite status

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import { DiscordInviteStatusV9, getInviteStatusV9 } from './api/v9';
+
+// Export utilities and types
+export * from './tools';
+
+// Export all API versions
+export * from './api/v9';
+
+
+/**
+ * Represents the status of a Discord invite.
+ * 
+ * This type is an alias for the latest version of the Discord invite status.
+ * For compatibility with previous versions, use the version-specific types such as DiscordInviteStatusV9, etc.
+ *
+ * @see DiscordInviteStatusV9
+ */
+type DiscordInviteStatus = DiscordInviteStatusV9;
+
+/**
+ * Retrieves the status of a Discord invite by its ID.
+ * 
+ * This function is a wrapper for the latest version of the Discord API (**V9**).
+ * For compatibility with previous versions, use the version-specific functions such as getInviteStatusV9, etc.
+ *
+ * @param inviteId - The unique identifier of the Discord invite.
+ * @returns A promise that resolves to the status of the Discord invite.
+ */
+export function getInviteStatus(inviteId: string): Promise<DiscordInviteStatus> {
+    return getInviteStatusV9(inviteId);
+}
