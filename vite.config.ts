f3fcb9bceda70c041cbc79143849992124a32522
﻿ <98918089+Oignontom8283@users.noreply.github.com>
2025-07-15 04:02:24 +0200

    Refactor Vite configuration to use hardcoded library name and filename

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
-import fs from 'fs';
-const packageJson:{name:string} = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf-8'));
 
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: packageJson.name,
-      fileName: (format) => `${packageJson.name}.${format}.js`,
+      name: "discordguildpeek",
+      fileName: (format) => `discord-guildpeek.${format}.js`,
       formats: ['umd', 'es'], // UMD pour navigateur global, ES pour moderne
     },
     outDir: 'dist/browser',
